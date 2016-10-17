1a981f425193b01b932878bf6afc9c20b16ef013
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2016-10-17 11:30:59 -0400

    added support for cli

--- pyasyncmail/cli.py
+++ pyasyncmail/cli.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 
 import click
+import asyncio
+from pyasyncmail import pyasyncmail 
+from pyasyncmail.pyasyncmail import main_mail
 
 
 @click.command()
 def main(args=None):
     """Console script for pyasyncmail"""
-    click.echo("Replace this message by putting your code into "
-               "pyasyncmail.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+    click.echo("Will start sending to all the people")
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main_mail())
+    click.echo("all mails sent")
 
 
 if __name__ == "__main__":
     main()
+
--- pyasyncmail/pyasyncmail.py
+++ pyasyncmail/pyasyncmail.py
@@ -1,41 +1,36 @@
 # -*- coding: utf-8 -*-
-import csvimport asyncio
+import csv
+import asyncio
 import smtplib
-from email.mime.text
-import MIMEText
+from email.mime.text import MIMEText
 
 
 def _open_file():
-    with open('reciepients_file.csv', newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-        return [', '.join(x) for x in spamreader]
+    with open('reciepients_file.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        return [', '.join(x) for x in spamreader]
 
 
 async def mail_funcs(each_person):
-    with open("mailfile.txt") as fp:
-        # Create a text/plain message
-        msg = MIMEText(fp.read())
-    
-    me = "joydeepubuntu@gmail.com"
-    you = each_person.split(",")[1]
-    msg['Subject'] = 'The contents of mailfile.txt'
-    msg['From'] = me
-    msg['To'] = you
-    # Send the message via our own SMTP server.
+    with open("mailfile.txt") as fp:
+        # Create a text/plain message
+        msg = MIMEText(fp.read())
 
-    s = smtplib.SMTP('localhost')
-    s.send_message(msg)
-    s.quit()
+    me = "joydeepubuntu@gmail.com"    
+    you = each_person.split(",")[1]
+    msg['Subject'] = 'The contents of mailfile.txt'
+    msg['From'] = me
+    msg['To'] = you
+    # Send the message via our own SMTP server.
 
-
-async def main():
-    fs = []
-    for each_person in _open_file():
-        fs.append(mail_funcs(each_person))
-
-    await asyncio.wait(fs)
+    s = smtplib.SMTP('localhost')
+    s.send_message(msg)
+    s.quit()
 
 
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+async def main_mail():
+    fs = []
+    for each_person in _open_file():
+        fs.append(mail_funcs(each_person))
+
+    await asyncio.wait(fs)
